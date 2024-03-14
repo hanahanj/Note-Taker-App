447d3033559b9dc6d63e2711623ee260c486cb84
Jonathan Hanahan <jonathan.hanahan@gmail.com>
2024-03-14 16:00:45 -0500

    connected notes.html, added .gitignore

---
+++ server.js
@@ -0,0 +1,22 @@
+console.log('server restart');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.send('Navigate to /notes'));
+
+app.get('/index', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
